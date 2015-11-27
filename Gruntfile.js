7215c1dfa30c07e79917dfe54876de0504f7e0e0
EvgeniZhibrik <evgeni.zhibrik@gmail.com>
2015-11-27 18:41:17 +0300

    enother try

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
             }
         },
         lintspaces: {
-            src: ['app/**/*.js','*.py','*.css', '*.ex'],
+            src: ['app/**/*.js','*.css', '*.ex'],
             options: {
                 editorconfig: '.editorconfig'
             },
