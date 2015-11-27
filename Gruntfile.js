744a0ec595c402041390e87196a1f7df49997b0f
EvgeniZhibrik <evgeni.zhibrik@gmail.com>
2015-11-27 18:19:48 +0300

    hhhoooo

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function(grunt) {
             }
         },
         lintspaces: {
-            src: ['app/**/*.js','*.html'],
+            src: ['app/**/*.js'],
             options: {
                 editorconfig: '.editorconfig'
             },
